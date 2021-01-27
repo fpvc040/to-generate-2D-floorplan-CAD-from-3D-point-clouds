3811cba61cecbe50a887e4591bdf4f6decfc59bd
Taru Rustagi <taru0904@umd.edu>
2021-01-27 11:57:53 -0500

    working installation

---
+++ CMakeFiles/floorplan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/floorplan.dir/floorplan.cpp.o"
+  "floorplan"
+  "floorplan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/floorplan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
